68ec043ec3f75a4610cf16f5137abb57e72aa810
Shandia <jaumeflorit05@gmail.com>
2025-06-12 10:04:43 +0200

    Fix: corregido uso incrrecto de bycrpt por pwd_context.hash

--- app/crud.py
+++ app/crud.py
@@ -1,3 +1,4 @@
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from . import models, schemas
 from passlib.hash import bcrypt
@@ -9,7 +10,7 @@ def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = bcrypt.hash(user.password)
+    hashed_password = pwd_context.hash(user.password)
     db_user = models.User(
         email=user.email,
         hashed_password=hashed_password,
